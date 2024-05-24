6094978ce30d4b8eebd42c6429877d83bd90f14c
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2024-05-24 12:33:57 -0700

    Updated logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,26 +15,26 @@ export default async function Home() {
         <div className="container">
           <div className="intro">
             <img
-              src="/imgs/alsiam.png"
+              src="/imgs/logo.png"
               alt="Al Siam Profile"
               className="shadow-dark"
             />
             <h1>Nicholas Dimidik</h1>
             <p>The <b className="full-text ">FULL</b> Stack Developer</p>
             <div className="social-links">
-              <a href="https://twitter.com/" target="_blank">
+              {/* <a href="https://twitter.com/" target="_blank">
                 <i className="fa fa-twitter" />
-              </a>
-              <a href="https://facebook.com/" target="_blank">
+              </a> */}
+              <a href="https://www.facebook.com/nick.dickerman/" target="_blank">
                 <i className="fa fa-facebook" />
               </a>
-              <a href="https://github.com/" target="_blank">
+              <a href="https://github.com/StickOnAStick" target="_blank">
                 <i className="fa fa-github" />
               </a>
-              <a href="https://instagram.com/" target="_blank">
+              <a href="https://www.instagram.com/nicholas_dimidik/" target="_blank">
                 <i className="fa fa-instagram" />
               </a>
-              <a href="https://linkedin.com/in/" target="_blank">
+              <a href="https://www.linkedin.com/in/nicholas-dimidik-71b20b159/" target="_blank">
                 <i className="fa fa-linkedin" />
               </a>
             </div>

18c3d0fdeb89ac5a5e0f987f2ab8d49fd4e392b5
StickOnAStick <StickOnAStick>
2024-06-05 10:35:42 -0700

    Fixed alignment issues with ImageNText component

--- src/app/portfolio/proj/socialstatus/page.tsx
+++ src/app/portfolio/proj/socialstatus/page.tsx
@@ -39,46 +39,75 @@ export default function ProjSocialStatus(){
                         </p>
                         <h4 className="text">Storage</h4>
                         <p className="project-p">
-                            Similar to the webapp framework choice I&apos;d opted for <b>Pocketbase</b> as it has a built-in s3 bucket storage and it was perfect for handling mass fetches. While the upload performance wasn&apos;t as great, PocketBase would be more than capable to handle a camera uplading 60 photos per second. 
+                            Not needing much in the way of storage I&apos;d opted for <b>Pocketbase</b> as it has a built-in s3 bucket storage and is performant when handling mass reads. While the upload performance wasn&apos;t as great, PocketBase would be more than capable to handle a camera uplading 60 photos per second. 
                         </p>
                         <h4 className="text">Cache</h4>
                         <p className="project-p">
-                            Since we needed to make predictions over a time series, that is, the previous photo(s) change the weights of the current prediction I needed a way to quickly  
+                            Since we needed to make predictions over a time series, that is, the previous photo(s) change the weights of the current prediction I needed a way to quickly store and release the last N frames of the video stream. <b>Redis</b> perfectly fit this role as I&apos;d be able to quickly set the incoming frames and freeing the previous frames automatically both in O(1) time.
                         </p>
                         <h4 className="text">Backend</h4>
                         <p className="project-p">
-                            Web sockets would be needed to connect all users to a single game instance, keeping state synced for every user. <b>SocketIO</b> was chosen for it&apos;s vast community and TypeScript implimentation allowing for a single-language application.
+                            Despite having experience with Flask and it working for the job at hand, I&apos;d opted to use <b>Fast API</b> for it&apos;s faster response times, integrated web-sockets / async made this an obvious choice.
                         </p>
                         <h4 className="text">AI Framework</h4>
                         <p className="project-p">
-                            PyTorch was used despite having prior experience with TensorFlow as I&apos;d wanted to explore more offerings & it was the framework of choice for Tesla&apos;s AutoPilot vision <b>public knowledge don&apos;t sue :)</b>.
+                            PyTorch was used despite having prior experience with TensorFlow as I&apos;d wanted to explore more offerings & it was the framework of choice for Tesla&apos;s AutoPilot vision.
                         </p>
                     </div>
                     <div className="project-tech-banner" style={{marginBottom: '15px'}}>
                         <Image className="project-image" src="/imgs/logos/next.png" width={300} height={300} alt="next JS Logo"/>
-                        <Image className="project-image" src="/imgs/logos/pb.png" width={300} height={300} alt="next JS Logo"/>
-                        <Image className="project-image" src="/imgs/logos/socketio.png" width={300} height={300} alt="next JS Logo"/>
+                        <Image className="project-image" src="/imgs/logos/pb.png" width={300} height={300} alt="Pocketbase Logo"/>
+                        <Image className="project-image" src="/imgs/logos/fastapi.png" width={300} height={300} alt="FastAPI Logo"/>
                     </div>
-                    <div className="about-text padd-15" style={{width: '100%'}}>
-                        <h3 className="text" style={{marginBottom: '15px'}}>Hardware</h3>
-                        <ImageNText 
-                        text="While building the prototype a friend recommended taking my game a SJSU startup competition. 
-                            Despite initally only creating the game for other's enjoyment and experience, this was an opportunity to practice pitching and test the commercial viability of the game."
-                        second_text="The revenue stream I discovered was to allow users to create and sell their own ''prompt-packs'' via an in-game marketplace. Solving two issues in one stroke, content and revenue generation, I moved forward with this proposal for my pitch."
-                        right={true}
-                        video={true}
-                        img="/imgs/portfolio/SocialStatus/video.mp4"/>
-                        <p className="project-p">
-                            Ultimately making it to the final stage of the competition I&apos;d enjoyed pushing myself to do this despite originally having no intention to. Learning what VC&apos;s expectations are ultimately helped me in presenting future projects such as U-Fi to VC&apos;s and ultimately has .
+                   
+                    <div className="about-text padd-15" style={{width: "100%", marginBottom: "1rem"}}>
+                        <h3>Hardware</h3>
+                        <p>
+                            Originally opting to use a <b>Pi-Pico W</b> with a <b>SPI Camera</b> due to budget constraints and *theoretically acceptable performance for uploading images. This idea was quickly scrapped after running into many issues with the SPI interface and the manufacturer&apos;s recommended interpretation of the output bytes did not translate into a valid JPG image.
+                            <br/><br/>
+                            Due to this and my focus on the AI aspect of the project I&apos;d opted to use the recently released <b>Raspberry Pi 5</b> joined with a <b>Raspberry Pi-Cam V2</b>.
                         </p>
                     </div>
-                    <div className="about-text padd-15" style={{width: "100%"}}>
-                        <h3>Hardware</h3>
-                        <p>Originally opting to use a <b>Pi-Pico W</b> with a <b>SPI Camera</b> due to budget constraints and a *theoretical acceptable performance for uploading images.</p>
+                    <div className="project-tech-banner" style={{marginBottom: '15px'}}>
+                        <Image className="project-image" src="/imgs/portfolio/SocialStatus/picam.png" width={300} height={300} alt="Picam Image"/>
+                        <Image className="project-image" src="/imgs/logos/pb.png" width={300} height={300} alt="Raspberry Pi 5"/>
+                        <Image className="project-image" src="/imgs/logos/socketio.png" width={300} height={300} alt="Raspberry Pi Pico W"/>
+                    </div>
+
+                    <div className="about-text padd-15" style={{width: '100%'}}>
+                        <h3 className="project-header text" style={{marginBottom: '15px'}}>Video Stream Output</h3>
+                        <ImageNText 
+                        text={
+                            <>
+                                After sorting out the kinks with the PiCamera interface I was so invested I&apos;d wanted to stream bytes directly to the server, pulling the individual frames out from the stream for storage as SQL byte blobs. 
+                                However, my stern-headedness would only go so far, as I eventually ditched the over-engineered grandious notions and stuck with what worked, which ultimately looked something like FFMPEG with extra headers.
+                                <br/> <br/>
+                                In my implimentation the <b>Raspberry Pi 5</b> is solely responsible for image capture and my <b>Linux server</b> would be solely responsible for distribution of the AI-processed video stream to connected clients. 
+                            </>
+                        }
+                        right={false}
+                        video={true}
+                        img="/imgs/portfolio/SocialStatus/video.mp4"
+                        />
+                        <p className="" style={{marginBottom: '15px'}}>
+                            The isolation of responsiblities worked really in keeping things <b>FAST</b>, getting up to 60fps on the end client just via unbatched HTTP POST requests alone. 
+                            This also prevented catostophic failure if the camera host or server went down, as the server and Pi were not dependent on one another and would restart as soon as the other came back online.
+                        </p>
+                    </div>
+                    <div className="about-text padd-15" style={{width: "100%", marginBottom: "1rem"}}>
+                        <h3>Training</h3>
+                        <p>
+                            By far my favorite part of the process, training proved to be the hardest & most rewarding aspect of this project, even still in my free time there&apos;s an ongoing effort to perfect the model.   
+                        </p>
+                        <p>
+                            Originally opting to use a <b>Pi-Pico W</b> with a <b>SPI Camera</b> due to budget constraints and *theoretically acceptable performance for uploading images. This idea was quickly scrapped after running into many issues with the SPI interface and the manufacturer&apos;s recommended interpretation of the output bytes did not translate into a valid JPG image.
+                            <br/><br/>
+                            Due to this and my focus on the AI aspect of the project I&apos;d opted to use the recently released <b>Raspberry Pi 5</b> joined with a <b>Raspberry Pi-Cam V2</b>.
+                        </p>
                     </div>
                     <div className="about-text padd-15" style={{width: '100%'}}>
-                        <h3 className="project-header text" style={{marginBottom: '15px'}}><span className="project-subheader">Ban</span> and Conclusion</h3>
-                        <p className="project-p">
+                        {/* <h3 className="project-header text" style={{marginBottom: '15px'}}>What was <span className="project-subheader">really built</span> and Conclusion</h3> */}
+                        {/* <p className="project-p">
                             Releasing the game July 1st, 2023 and quickly advertising through the r/AuxBattlesGame subreddit. Continually posting to an unresponsive community, although uninspiring, did pay it&apos;s dividends with the game ultimately hitting 20k users & 1k recurring within a month.
                             With numbers providing motivation I prepared to launch the prompt-pack marketplace update to expand the games content and provide a revenue stream. Spotify had other plans however, formally banning be from using their developer service due to usage in a game on August 6th, a week before the slightly delayed August 15th release date.
                         </p>
@@ -89,7 +118,7 @@ export default function ProjSocialStatus(){
                         <p className="project-p">
                             Until... I&apos;d discovered that someone cloned my game. After finding and calling them for banter about such as them using the prompt-pack idea but using youtube&apos;s API and giving merit for a cool feature added wherein users could extend the currently playing track.
                             After our brief chat I&apos;d prospected if they&apos;d like to continue with development after I discovered a loop-hole in the Spotify TOS; however, despite my charm they declinded. Leaving me to decide if I should continue with development, fueled by competitive drive and desire to finish what was started. 
-                        </p>
+                        </p> */}
                     </div>
                 </div>
                 
--- src/components/project/img&text.tsx
+++ src/components/project/img&text.tsx
@@ -13,34 +13,32 @@ export default function ImageNText(props: ImageNTextProps){
             {
                 props.right ? 
             <>
-                <div className="projectImageNText-Item">
                 {
                     props.video ? 
-                    <video className="projectImageNText-wrapper" width={575} height={323} autoPlay controls loop preload="none">
+                    <video className="projectImageNText-image" width={575} height={323} autoPlay controls loop preload="none">
                         <source src={props.img} type="video/mp4"/>
                     </video>
                     :
-                    <Image className="projectImageNText-wrapper"  src={props.img} height={300} width={300} alt="concepts"/>
+                    <Image className="projectImageNText-image"  src={props.img} height={300} width={300} alt="concepts"/>
                 }
-                </div>
-                <div className="projectImageNText-content projectImageNText-Item project-p">
+                <div className="projectImageNText-text project-p">
                     <p>{props.text}</p>
                     <p>{props.second_text}</p>
                 </div>
             </>
             :
             <>
-                <div className="projectImageNText-content project-p">
+                <div className="projectImageNText-text project-p">
                     <p>{props.text}</p>
                     <p>{props.second_text}</p>
                 </div>
                 {
                     props.video ? 
-                    <video className="projectImageNText-wrapper" width={575} height={323} loop controls autoPlay preload="none">
+                    <video className="projectImageNText-image" width={575} height={323} loop controls autoPlay preload="none">
                         <source src={props.img} type="video/mp4"/>
                     </video>
                     :
-                    <Image className="projectImageNText-wrapper"  src={props.img} height={300} width={300} alt="concepts"/>
+                    <Image className="projectImageNText-image"  src={props.img} height={300} width={300} alt="concepts"/>
                 }
             </>
             }
